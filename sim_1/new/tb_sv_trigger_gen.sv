dba346e798ee2816589c92d4151f4ee498c22200
shrtique <shrt@d-test.ru>
2018-12-20 14:00:51 +0500

    all bullshit comments are deleted, stable ver

--- sim_1/new/tb_sv_trigger_gen.sv
+++ sim_1/new/tb_sv_trigger_gen.sv
@@ -16,13 +16,9 @@
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-<<<<<<< HEAD
-// gitted
-//git test once again
-//new_branch
-=======
+//
 // Branch: master
->>>>>>> master
+
 //////////////////////////////////////////////////////////////////////////////////
 
 
