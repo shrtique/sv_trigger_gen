03ceb2e2ccc807ab189f4bb731ac2d5dd034104a
shrtique <shrt@d-test.ru>
2018-12-20 12:39:28 +0500

    init

---
+++ sim_1/new/tb_sv_trigger_gen.sv
@@ -0,0 +1,74 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.12.2018 16:36:37
+// Design Name: 
+// Module Name: tb_sv_trigger_gen
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_sv_trigger_gen();
+
+//signsals
+logic clk;
+logic aresetn;
+
+logic ext_trig;
+logic enc_trig;
+
+logic en_enc;
+
+
+sv_trigger_gen JOPA(
+
+  .i_clk              ( clk      ),              
+  .i_aresetn          ( aresetn  ),          
+  
+  .ext_input          ( ext_trig ),          
+  .encoder_step_input ( enc_trig ), 
+
+  .en_enc_input       ( en_enc   ),       
+
+  .o_trigger          ( )           
+
+);
+
+
+//generate clk
+always
+  begin
+    clk = 1; #0.5;
+    clk = 0; #0.5;
+  end
+
+initial
+  begin
+
+    aresetn  = 0; ext_trig = 0;
+    enc_trig = 0; en_enc   = 0;
+    #10; 
+    aresetn = 1; #5;
+
+    ext_trig <= 1; #1;
+    ext_trig <= 0; #10;
+
+    en_enc <= 1; #10;
+    enc_trig <= 1; #1;
+    enc_trig <= 0; #10;
+    en_enc <= 0;
+
+  end  
+endmodule
---
+++ sources_1/new/sv_trigger_gen.sv
@@ -0,0 +1,70 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.12.2018 11:19:26
+// Design Name: 
+// Module Name: sv_trigger_gen
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sv_trigger_gen(
+
+  input  logic i_clk,              //system clock
+  input  logic i_aresetn,          //system reset 
+  
+  input  logic ext_input,          //for external trigger
+  input  logic encoder_step_input, //encoder trigger
+
+  input  logic en_enc_input,       //if '1' use encoder_step_input, if '0' use ext_input
+
+  output logic o_trigger           //output signal for trigger
+
+);
+
+//DESCRIPTION
+//Use this module to generate trigger signal for python1300_ip_core.
+//Here we don't have any debouncing circuits, only sync(two regs in a row for external trigger)
+
+logic       mux_trigger_signal;
+logic [0:1] ext_trigger_sync;
+
+//SYNC
+always_ff @ ( posedge i_clk, negedge i_aresetn ) begin
+
+    if ( ~i_aresetn ) begin  
+        ext_trigger_sync <= 2'b00;
+    end else begin
+        ext_trigger_sync <= {ext_input, ext_trigger_sync[0]};
+    end
+end
+
+//MUX
+assign mux_trigger_signal = en_enc_input ? encoder_step_input : ext_trigger_sync[1];
+
+
+//OUTPUT reg
+always_ff @ ( posedge i_clk, negedge i_aresetn ) begin
+
+    if ( ~i_aresetn ) begin  
+        o_trigger <= 1'b0;
+    end else begin
+        o_trigger <= mux_trigger_signal;
+    end
+end
+
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+endmodule
